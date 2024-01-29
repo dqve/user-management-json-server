d8d0fc4609c02fe06687b92e718562f8fd4bdbfc
David Ayoola <ayooladavid@gmail.com>
2024-01-29 07:00:45 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -20,7 +20,7 @@ server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
+    '/users/:id/show': '/:resource/:id'
 }))
 server.use(router)
 server.listen(3000, () => {
